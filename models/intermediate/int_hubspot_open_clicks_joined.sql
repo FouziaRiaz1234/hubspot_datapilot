b38e15b6d4bf7ee83c65a3134abd34e5ce6ec695
muhammad.usman@data-pilot.com <muhammad.usman@data-pilot.com>
2024-08-04 22:20:41 +0000

    materialized views for open and click event email tables

--- models/intermediate/int_hubspot_open_clicks_joined.sql
+++ models/intermediate/int_hubspot_open_clicks_joined.sql
@@ -22,11 +22,11 @@ open_events AS (
   FROM {{ ref('stg_hubspot_open_event') }}
 )
 
-SELECT DISTINCT
+SELECT
+  a.event_id,
   a.campaign_id,
   a.from_name,
   a.created_at,
-  a.event_id,
   a.device_type,
   a.event_type,
   a.sentBy_created,
@@ -38,8 +38,31 @@ SELECT DISTINCT
   COALESCE(c.device_type, o.device_type) AS event_device_type,
   COALESCE(c.sentby_id, o.sentby_id) AS event_sentby_id,
   COALESCE(c.sentby_created, o.sentby_created) AS event_sentby_created,
-  o.duration AS open_duration 
+  MAX(o.duration) AS open_duration -- This field will be NULL for click events
 FROM
   all_email_events a
-  LEFT JOIN click_events c ON a.campaign_id = c.campaign_id AND a.event_type = 'CLICK'
-  LEFT JOIN open_events o ON a.campaign_id = o.campaign_id AND a.event_type = 'OPEN'
+  LEFT JOIN click_events c ON a.event_id = c.event_id AND a.event_type = 'CLICK'
+  LEFT JOIN open_events o ON a.event_id = o.event_id AND a.event_type = 'OPEN'
+GROUP BY
+  a.event_id,
+  a.campaign_id,
+  a.from_name,
+  a.created_at,
+  a.device_type,
+  a.event_type,
+  a.sentBy_created,
+  a.category,
+  c.recipient,
+  c.country,
+  c.state,
+  c.city,
+  c.device_type,
+  c.sentby_id,
+  c.sentby_created,
+  o.recipient,
+  o.country,
+  o.state,
+  o.city,
+  o.device_type,
+  o.sentby_id,
+  o.sentby_created
